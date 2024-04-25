67da949f38d9ac93ecb6ca004db95a69ffcdaa67
ChithraB009 <167099209+ChithraB009@users.noreply.github.com>
2024-04-25 10:15:20 +0530

    tree traversal.py

---
+++ tree traversal.py
@@ -0,0 +1,35 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data 
+        self.left = None 
+        self.right = None 
+ 
+def printPreOrder(root):
+    if root == None:
+        return
+ 
+    # root, left_subtree, right_subtree 
+    print(root.data, end = " ")
+    printPreOrder(root.left)
+    printPreOrder(root.right)
+ 
+def printInOrder(root):
+    if root == None:
+        return
+ 
+    # left_subtree, root, right_subtree 
+ 
+    printInOrder(root.left)
+    print(root.data, end = " ")
+    printInOrder(root.right) 
+ 
+def printPostOrder(root):
+    if root == None:
+        return
+ 
+    # left_subtree, right_subtree, root
+ 
+    printPostOrder(root.left)
+    printPostOrder(root.right) 
+    print(root.data, end = " ")
+ 
